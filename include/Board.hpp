82d54666c9d6b0cb5d0e78806ef59e5ab2fdd0f7
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2019-05-22 22:14:41 +0200

    Witching players when doing a move

--- include/Board.hpp
+++ include/Board.hpp
@@ -4,26 +4,47 @@
 #include <cstdint>
 
 const std::array<uint64_t, 8> columns = {
-    0x0101010101010101,
-    0x0202020202020202,
-    0x0404040404040404,
-    0x0808080808080808,
-    0x1010101010101010,
-    0x2020202020202020,
-    0x4040404040404040,
-    0x8080808080808080
+	0x0101010101010101,
+	0x0202020202020202,
+	0x0404040404040404,
+	0x0808080808080808,
+	0x1010101010101010,
+	0x2020202020202020,
+	0x4040404040404040,
+	0x8080808080808080
 };
 
-enum Color { WHITE = 0, BLACK = 1, BOTH = 2 };
+enum class Color { White, Black, BOTH };
+
+inline Color otherPlayer(const Color side) {
+	if (side == Color::White) {
+		return Color::Black;
+	}
+	return Color::White;
+}
+
+inline size_t playerToIndex(const Color player) {
+	if (player == Color::White) {
+		return 0;
+	}
+	return 1;
+}
 
 class Board
 {
-private:
-    std::array<uint64_t, 3> board;
-    uint64_t tops;
-public:
-    Board(const std::array<uint64_t, 3> board, const uint64_t tops);
-    static Board empty();
+	private:
+		std::array<uint64_t, 3> board;
+		uint64_t tops;
+		Color currentPlayer;
 
-    void doMove(const uint8_t column, const Color side);
+		void switchPlayer();
+	public:
+		Board(const std::array<uint64_t, 3> board, const uint64_t tops);
+		static Board empty();
+
+		void doMove(const uint8_t column, const Color side);
 };
+
+inline void Board::switchPlayer() {
+	this->currentPlayer = otherPlayer(this->currentPlayer);
+}
--- src/Board.cpp
+++ src/Board.cpp
@@ -2,22 +2,23 @@
 
 #include <cassert>
 
-Board::Board(const std::array<uint64_t, 3> board, const uint64_t tops) : board(board), tops(tops) {}
+Board::Board(const std::array<uint64_t, 3> board, const uint64_t tops) : board(board), tops(tops), currentPlayer(Color::White) {}
 
 Board Board::empty() {
-    const std::array<uint64_t, 3> board = { 0, 0, 0 };
-    const uint64_t tops = 0;
-    return Board(board, tops);
+	const std::array<uint64_t, 3> board = { 0, 0, 0 };
+	const uint64_t tops = 0;
+	return Board(board, tops);
 }
 
 void Board::doMove(const uint8_t column, const Color side) {
-    assert(column >= 0);
-    assert(column < 8);
+	assert(column >= 0);
+	assert(column < 8);
 
-    const uint64_t top = this->tops & columns[column];
+	const uint64_t top = this->tops & columns[column];
 
-    this->board[side] |= top;
-    this->board[2] |= top;
+	this->board[playerToIndex(side)] |= top;
+	this->board[2] |= top;
 
-    this->tops ^= top | (top << 8);
+	this->tops ^= top | (top << 8);
+	this->switchPlayer();
 }
