fcc08cf5b53e7eb89415c53be5b428c2a7bbf4fb
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2019-05-30 10:48:51 +0200

    Add visits and scores to each node to keep track of the weight of the node

--- include/Node.hpp
+++ include/Node.hpp
@@ -7,6 +7,10 @@ private:
 	Board board;
 	std::queue<Move> movesLeft;
 	std::vector<Node> children;
+
+	float score;
+	int visits;
+	int winScore;
 public:
 	Node(const Board& board);
 };
--- src/Node.cpp
+++ src/Node.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <random>
 
-Node::Node(const Board& board) : board(board) {
+Node::Node(const Board& board) : board(board), score(0.0), visits(0), winScore(0) {
 	std::vector<Move> moves = this->board.moveList();
 	auto rng = std::default_random_engine {};
 	std::shuffle(std::begin(moves), std::end(moves), rng);
